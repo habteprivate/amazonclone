8c5a8d6aef7cf001470379a81cf9a8295b9bafe8
Habtamu <73044474+habteprivate@users.noreply.github.com>
2024-09-26 23:02:25 -0400

    backend deployed

--- src/API/Axios.js
+++ src/API/Axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8061",
+  // local host
+  // baseURL: "http://127.0.0.1:5001/clone-8b30b/us-central1/app",
+  // firebase
+  // baseURL: "http://localhost:8061",
+  // render
+  baseURL: "https://amazon-api-wnwu.onrender.com",
 });
 
 export { axiosInstance };
